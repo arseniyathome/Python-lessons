cc9f3b68bded02771568a10ec9d7b17c555540be
Arseniy at home <arseniy@vpkr.site>
2024-04-11 22:25:23 +0500

    selection sort added

--- selection-sort.py
+++ selection-sort.py
@@ -1,8 +1,18 @@
 # Сортировка методом поиска
-import random_array
+import lib.random_array as random_array
 
-myArray = random_array.create(0,10,10)
+myArray = random_array.create(0,100,10)
 
 print(myArray)
 
+n = len(myArray)
+
+for i in range(n):
+    min_index = i
+    for j in range(i+1, n):
+        if myArray[j] < myArray[min_index]:
+            min_index = j
+    myArray[i], myArray[min_index] = myArray[min_index], myArray[i]
+
+print(myArray)
 
