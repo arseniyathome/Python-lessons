372a9efa0adcf618d85286bc52a3719083bebbe8
Arseniy at home <arseniy@vpkr.site>
2024-04-11 21:56:35 +0500

    random_array module added

---
+++ random_array.py
@@ -0,0 +1,8 @@
+import random
+
+def create(min_value, max_value, array_size):
+    return [random.randint(min_value, max_value) for _ in range(array_size)] 
+
+
+
+
--- selection-sort.py
+++ selection-sort.py
@@ -1,1 +1,8 @@
 # Сортировка методом поиска
+import random_array
+
+myArray = random_array.create(0,10,10)
+
+print(myArray)
+
+
